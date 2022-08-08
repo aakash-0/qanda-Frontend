731484fc25d21b59201dcdd601ec3fe1dc6f284e
Aakash Yadav <yaakash59.ay@gmail.com>
2022-08-08 11:06:12 +0530

    added logos and css in header

---
+++ src/components/Quora.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import QuoraHeader from './QuoraHeader'
+import "./css/Quora.css"
+function Quora() {
+  return (
+    <div className='quora'>
+        <QuoraHeader/>
+    </div>
+  )
+}
+
+export default Quora
---
+++ src/components/QuoraHeader.js
@@ -0,0 +1,118 @@
+import React, { useState } from "react";
+import HomeIcon from "@material-ui/icons/Home";
+import FeaturedPlayListOutlinedIcon from "@material-ui/icons/FeaturedPlayListOutlined";
+import {
+  AssignmentTurnedInOutlined,
+  // Close,
+  NotificationsOutlined,
+  PeopleAltOutlined,
+  Search,
+  ExpandMore,
+} from "@material-ui/icons";
+import CloseIcon from "@material-ui/icons/Close";
+import { Avatar, Button, Input } from "@material-ui/core";
+import "./css/QuoraHeader.css";
+import { Modal } from "react-responsive-modal";
+import "react-responsive-modal/styles.css";
+
+
+
+function QuoraHeader() {
+  
+
+    
+
+  
+  return (
+    <div className="qHeader">
+      <div className="qHeader-content">
+        <div className="qHeader__logo">
+          <img
+            src="https://video-public.canva.com/VAD8lt3jPyI/v/ec7205f25c.gif"
+            alt="logo"
+          />
+        </div>
+        <div className="qHeader__icons">
+          <div className="qHeader__icon">
+            <HomeIcon />
+          </div>
+          <div className="qHeader__icon">
+            <FeaturedPlayListOutlinedIcon />
+          </div>
+          <div className="qHeader__icon">
+            <AssignmentTurnedInOutlined />
+          </div>
+          <div className="qHeader__icon">
+            <PeopleAltOutlined />
+          </div>
+          <div className="qHeader__icon">
+            <NotificationsOutlined />
+          </div>
+        </div>
+        <div className="qHeader__input">
+          <Search />
+          <input type="text" placeholder="Search questions" />
+        </div>
+        <div className="qHeader__Rem">
+          <span >
+            <Avatar  />
+          </span>
+
+          <Button>Add Question</Button>
+          <Modal
+            
+          >
+            <div className="modal__title">
+              <h5>Add Question</h5>
+              <h5>Share Link</h5>
+            </div>
+            <div className="modal__info">
+              <Avatar  className="avatar" />
+              <div className="modal__scope">
+                <PeopleAltOutlined />
+                <p>Public</p>
+                <ExpandMore />
+              </div>
+            </div>
+            <div className="modal__Field">
+              <Input
+                
+                type=" text"
+                placeholder="Start your question with 'What', 'How', 'Why', etc. "
+              />
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                }}
+              >
+                <input
+                  type="text"
+                 
+                  style={{
+                    margin: "5px 0",
+                    border: "1px solid lightgray",
+                    padding: "10px",
+                    outline: "2px solid #000",
+                  }}
+                  placeholder="Optional: inclue a link that gives context"
+                />
+                
+              </div>
+            </div>
+            <div className="modal__buttons">
+              <button className="cancle" >
+                Cancel
+              </button>
+              <button  type="submit" className="add">
+                Add Question
+              </button>
+            </div>
+          </Modal>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default QuoraHeader;
